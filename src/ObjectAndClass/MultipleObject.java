d567da4acf519d3d110e10397eb11a36d5f0e0d4
CodeMakerByKoo <koo_by@hotmail.com>
2023-03-24 00:09:33 +0800

    New statement

--- src/ObjectAndClass/MultipleObject.java
+++ src/ObjectAndClass/MultipleObject.java
@@ -1,4 +1,32 @@
 package ObjectAndClass;
 
-public class MultipleObject {
+class MultipleObject {
+
+    int length;
+    int width;
+
+    void insert(int l, int w){
+
+        length = l;
+        width = w;
+    }
+
+    void calculateArea(){
+
+        System.out.println(length * width);
+    }
+}
+
+class TestRectangle{
+
+    public static void main(String[] args) {
+
+        MultipleObject m1 = new MultipleObject(), m2 = new MultipleObject(); // Creating two objects
+
+        m1.insert(11, 5);
+        m2.insert(3, 15);
+
+        m1.calculateArea();
+        m2.calculateArea();
+    }
 }
